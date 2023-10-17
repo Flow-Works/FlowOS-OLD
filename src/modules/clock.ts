570f025515e353d1d576c4a7aebfd6725624d19a
WhoAboutYT <63606830+idk-pixel@users.noreply.github.com>
2023-10-16 20:29:05 -0500

    [🔥] Fix Clock
    Fixes the clock to make it actually work, uses Date first and British/Military time

--- src/modules/clock.ts
+++ src/modules/clock.ts
@@ -7,5 +7,32 @@ export const meta = {
 export const run = (element: HTMLDivElement): void => {
   element.style.display = 'flex'
   element.style.alignItems = 'center'
-  element.innerText = '9:41 AM\n10/14/2023'
+
+  const refreshDate = (): any => {
+    const split = date.toLocaleDateString('en-US', { weekday: 'long', month: 'long', day: 'numeric' }).split(',')
+    return `<i>${split[0]}</i>,${split[1]} `
+  }
+
+  const refreshClock = (): any => {
+    return date.toLocaleTimeString('en-US', { hour12: false, hour: 'numeric', minute: 'numeric' })
+  }
+
+  let date: Date = new Date()
+
+  refreshDate()
+  refreshClock()
+
+  setInterval(() => {
+    date = new Date()
+    const date_: string = refreshDate()
+    element.innerHTML = `${date_} <br><span id="time">,  </span>`
+  }, 1000)
+  setInterval(() => {
+    date = new Date()
+    const clock: string = refreshClock()
+    const time = document.getElementById('time')
+    if (time !== null) {
+      time.innerText = ` | ${clock}`
+    }
+  }, 1000)
 }
