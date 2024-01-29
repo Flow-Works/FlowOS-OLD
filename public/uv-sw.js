ad38f19f2967012d0b715978d89f107103e6fc96
ThinLiquid <thnlqd@gmail.com>
2024-01-29 09:53:13 +0000

    feat: use UV version 1.0.11

--- public/uv-sw.js
+++ public/uv-sw.js
@@ -1,9 +1,21 @@
-// @ts-nocheck
+/*global UVServiceWorker,__uv$config*/
+/*
+ * Stock service worker script.
+ * Users can provide their own sw.js if they need to extend the functionality of the service worker.
+ * Ideally, this will be registered under the scope in uv.config.js so it will not need to be modified.
+ * However, if a user changes the location of uv.bundle.js/uv.config.js or sw.js is not relative to them, they will need to modify this script locally.
+ */
 
-importScripts('/uv/uv.sw.js');
+
+importScripts(`uv.config.js`);
 
 const params = new URL(self.location.href).searchParams
 const serverURL = atob(params.get('url'));
-const sw = new UVServiceWorker(serverURL);
+self.__uv$config.bare = `${serverURL}/bare/`
 
-self.addEventListener('fetch', event => event.respondWith(sw.fetch(event)));
+importScripts('uv.bundle.js');
+importScripts(__uv$config.sw || 'uv.sw.js');
+
+const sw = new UVServiceWorker();
+
+self.addEventListener('fetch', (event) => event.respondWith(sw.fetch(event)));
