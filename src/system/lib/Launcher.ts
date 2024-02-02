aa5fb9e29082b25e68a7a3648319d97056642666
ThinLiquid <thnlqd@gmail.com>
2024-02-02 04:02:09 +0000

    feat: created start menu
    re #183

--- src/system/lib/Launcher.ts
+++ src/system/lib/Launcher.ts
@@ -9,25 +9,34 @@ const Launcher: Library = {
     targetVer: '1.0.0-indev.0'
   },
   init: (l, k) => {
-    Launcher.data.element = new l.HTML('launcher').style({
-      opacity: '0',
-      'backdrop-filter': 'blur(0px)',
-      'pointer-events': 'none'
+    Launcher.data.element = new l.HTML('div').style({
+      background: `${document.documentElement.style.getPropertyValue('--mantle')}E0`,
+      width: '350px',
+      height: '500px',
+      margin: '20px',
+      padding: '10px',
+      overflow: 'hidden',
+      'border-radius': '20px',
+      border: '1px solid var(--surface-0)',
+      position: 'fixed',
+      bottom: '-500px',
+      left: '0',
+      transition: 'bottom 0.5s cubic-bezier(1,0,0,1)',
+      'z-index': '1000',
+      'box-shadow': '0 0 10px 0 rgba(0,0,0,0.5)',
+      'user-select': 'none',
+      'backdrop-filter': 'blur(10px)'
     })
   },
   data: {
     toggle: () => {
       if (isLauncherOpen) {
         Launcher.data.element.style({
-          opacity: '0',
-          'backdrop-filter': 'blur(0px)',
-          'pointer-events': 'none'
+          bottom: '-500px'
         })
       } else {
         Launcher.data.element.style({
-          opacity: '1',
-          'backdrop-filter': 'blur(10px)',
-          'pointer-events': 'all'
+          bottom: '68px'
         })
       }
 
